6a4375ad58bfb8cc875fb2613fe4e514259ce1b1
Ajay Saini <ajaysaini.sgvu@gmail.com>
2017-09-13 11:16:29 +0530

    Refactor ticker.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require("serve-favicon");
 var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
+var fs = require("fs");
 
 var index = require("./routes/index");
 var users = require("./routes/users");
@@ -17,6 +18,7 @@ app.set("view engine", "jade");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/ticker.js
+++ routes/ticker.js
@@ -1,104 +1,52 @@
 const express = require("express");
 const router = express.Router();
-var rp = require("request-promise");
+const rp = require("request-promise");
 const cheerio = require("cheerio");
+const CircularJSON = require("circular-json");
+
 const { rollbar } = require("../config/rollbar");
 const { baseUrl } = require("../config/settings");
 
 /** API to get all currency price */
 router.get("/:ticker?", function(req, res, next) {
-  var currencyList = [];
-
   // it can be null as ticker is optional parameter
   let reqCurrencyName = req.params.ticker;
+
+  const currencyList = [];
 
   rp(baseUrl)
     .then(body => {
       const $ = cheerio.load(body);
-      $("tr").each(function name(i, elem) {
-        // currency name
-        var currencyName = $(this)
+      const getField = (elem, orderNumber) =>
+        $(elem)
           .find("td")
-          .eq(1)
+          .eq(orderNumber)
           .text()
           .replace(/\r?\n|\r/g, " ")
           .trim();
+      const currencies = $("tr")
+        .filter((i, e) => {
+          // Filter rows with no currency name
+          return Boolean(getField(e, 1));
+        })
+        .map((i, e) => ({
+          // Construct the object
+          currencyName: getField(e, 1),
+          marketCap: getField(e, 2),
+          price: getField(e, 3),
+          dataSupply: getField(e, 4).replace(/ /g, ""),
+          volume: getField(e, 5),
+          negativeChange: getField(e, 6)
+        }));
 
-        // market cap value
-        var marketCap = $(this)
-          .find("td")
-          .eq(2)
-          .text()
-          .replace(/\r?\n|\r/g, " ")
-          .trim();
-
-        // currency price
-        var price = $(this)
-          .find("td")
-          .eq(3)
-          .text()
-          .replace(/\r?\n|\r/g, " ")
-          .trim();
-
-        // date supply value
-        var dataSupply = $(this)
-          .find("td")
-          .eq(4)
-          .text()
-          .replace(/\r?\n|\r/g, " ")
-          .trim()
-          .replace(/ /g, "");
-
-        // volume value
-        var volume = $(this)
-          .find("td")
-          .eq(5)
-          .text()
-          .replace(/\r?\n|\r/g, " ")
-          .trim();
-
-        // negative change value
-        var negativeChange = $(this)
-          .find("td")
-          .eq(6)
-          .text()
-          .replace(/\r?\n|\r/g, " ")
-          .trim();
-
-        // first item is empty in response so lets not push it into an array
-        if (currencyName !== "") {
-          if (reqCurrencyName !== undefined) {
-            if (currencyName === reqCurrencyName) {
-              const currencyItem = {
-                currencyName: currencyName,
-                marketCap: marketCap,
-                price: price,
-                dataSupply: dataSupply,
-                volume: volume,
-                negativeChange: negativeChange
-              };
-
-              currencyList.push(currencyItem);
-            }
-          } else {
-            const currencyItem = {
-              currencyName: currencyName,
-              marketCap: marketCap,
-              price: price,
-              dataSupply: dataSupply,
-              volume: volume,
-              negativeChange: negativeChange
-            };
-
-            currencyList.push(currencyItem);
-          }
-        }
-      });
-      res.send(currencyList);
+      // Respond
+      res.send(currencies.get());
     })
-    .catch(err => {
-      rollbar.log(err);
-      res.send({ error: err });
+    .catch(error => {
+      rollbar.log(error);
+      res.send({
+        error
+      });
     });
 });
 
